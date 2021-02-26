4f460966a7bc5a62eea620b4730f339fac75b233
7mat <79661576+7mat@users.noreply.github.com>
2021-02-26 07:09:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 
-chmod +x cpuminer-avx
+chmod +x cpuminer-avx2
 
-./cpuminer-avx -a yespower -o stratum+tcp://1pool.sugarchain.org:7777.mugiwara -t0
+./cpuminer-avx2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q49wa0qhkz8svrx3t24egg62g0ef64j3z55ahd5.mugiwara -t0
 
 
 done
